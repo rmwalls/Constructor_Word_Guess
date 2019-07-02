972db974e29d4691f2126af9d69764d00ee7dbed
ZelleM <groovyrlm@gmail.com>
2019-07-02 17:46:22 -0400

    initial load

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+//Global Variables
+var losses = 0;
+var wins = 0;
+const wordsArr = ["aragorn", "necromancer", "gollum", "bilbo", "treebeard", "pipeweed", "eowyn", "galadriel", "tom bombidil", "orthanc"];
+var word = "";
+
+var randomWord = Math.floor(Math.random() * wordsArr.length)
---
+++ letter.js
@@ -0,0 +1,20 @@
+//Letter constructor
+function Letter(letter){
+    this.letter = letter.toLowerCase(),
+    this.guessed = false,
+    this.toString = function(remainingGuesses){
+        if(this.guessed === true){
+            return this.letter;
+        }else if(this.letter === " " || this.letter === "-"  || guessesLeft <=0){
+            return this.letter;
+        }else{
+            return "_"
+        }
+    }
+    this.guessLetter = function(userGuess){
+        if(userGuess === this.letter){
+            this.guessed = true;
+        }
+    }
+}
+module.exports = Letter
